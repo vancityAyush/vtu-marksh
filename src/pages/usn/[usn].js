896e8eff763516bb28ed9c42c5e793f235a42612
vancityAyush <eliasakeleven@gmail.com>
2023-07-31 20:47:20 +0530

    changes

--- src/pages/usn/[usn].js
+++ src/pages/usn/[usn].js
@@ -1,12 +1,23 @@
-import { useRouter } from 'next/router';
+import {useRouter} from 'next/router';
 
-export default function UserPage() {
-  const router = useRouter();
-  const { usn } = router.query;
+export default function UserPage({data}) {
+    const router = useRouter();
+    const {usn} = router.query;
 
-  return (
-    <div>
-      <h1>Your USN: {usn}</h1>
-    </div>
-  );
+    return (
+        <div>
+            <h1>Your USN: {usn}</h1>
+        </div>
+    );
+}
+
+export async function getServerSideProps() {
+    const data = await fetch(
+        "/usn"
+    );
+    return {
+        props: {
+            data,
+        },
+    };
 }
