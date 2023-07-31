0464dffc334bc47da567b5bfe4a464cbd6d24951
vancityAyush <eliasakeleven@gmail.com>
2023-07-31 20:49:49 +0530

    changes

---
+++ src/pages/api/result.js
@@ -0,0 +1,42 @@
+import {token} from "@/lib/constants";
+
+export default async function handler(req, res) {
+    const {usn} = req.query;
+    const data = await fetchResult(usn);
+    const result = await data.json();
+    console.log(result);
+    const sub_result = [];
+    for (const subject of result) {
+        const sem = await fetchSem({usn: subject.usn, yearmonth: subject.resultMonthYear, sem: subject.semester});
+        const sem_result = await sem.json();
+        sub_result.push(sem_result);
+        console.log(sem_result);
+    }
+    // result.sub_result = sub_result;
+
+    res.status(200).json(result);
+}
+
+async function fetchResult(usn) {
+    return await fetch(
+        "https://api.vtuconnect.in/v2/result/" + usn,
+        {
+            headers: {
+                //Bearer Token
+                'Authorization': 'Bearer ' + token,
+            }
+        }
+    );
+}
+
+async function fetchSem({usn, yearmonth, sem}) {
+    return await fetch(
+        `https://api.vtuconnect.in/v2/result/sem?usn=${usn}&yearmonth=${yearmonth}&sem=${sem}`,
+        {
+            headers: {
+                //Bearer Token
+                'Authorization': 'Bearer ' + token,
+            },
+        }
+    );
+}
