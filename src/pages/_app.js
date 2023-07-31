e0184d442401817c8c9f644840d415cc241a0dd8
vancityAyush <eliasakeleven@gmail.com>
2023-07-31 20:57:00 +0530

    changes

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,12 @@
 import "@/styles/globals.css";
 import Layout from "@/layouts/Layout";
-import Script from "next/script";
 
-export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </>
-  );
+export default function App({Component, pageProps}) {
+    return (
+        <>
+            <Layout>
+                <Component {...pageProps} />
+            </Layout>
+        </>
+    );
 }
