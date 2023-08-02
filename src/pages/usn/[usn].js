2928fab73c58c23b7ecae0463a97406edb100fa4
vancityAyush <eliasakeleven@gmail.com>
2023-08-03 03:05:13 +0530

    v1

--- src/pages/usn/[usn].js
+++ src/pages/usn/[usn].js
@@ -3,7 +3,7 @@ import {NumberAnim} from "@/components/number_anim";
 
 export default function UserPage({data}) {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <main className="flex min-h-screen flex-col items-center justify-between p-24 w-screen overflow-hidden">
             <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
                 <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30 ">
                     VTU Results for {data.name}
@@ -22,7 +22,7 @@ export default function UserPage({data}) {
                 </div>
             </div>
 
-            <div className="mt-2 mb-2">
+            <div className="m-5">
                 {Object.values(data.sem_results).map((sem_result, index) => (
                     <div key={index} className="collapse bg-base-800 m-4 ">
                         <input type="checkbox" className="peer"/>
